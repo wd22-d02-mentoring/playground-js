24e931dd1a9ad05877d628401ca5eb7eb12d9ee4
losrobbos <losrobbos@web.de>
2022-07-01 11:18:26 +0200

    Add coffee counter sample

--- 19-count-items.js
+++ 19-count-items.js
@@ -10,16 +10,39 @@ const allCoffees = [
 ]
 
 // get COUNT of all coffees of a type
-// DATEN: allCoffees = [... coffees ...]
-// FUNCTION: countCoffees
-// INPUT: whole array to search in, coffeeType
-// OUTPUT: amount of items of that type
+// X DATEN: allCoffees = [... coffees ...]
+// X FUNCTION: countCoffees
+// X INPUT: whole array to search in, coffeeType to search for
+// OUTPUT: amount of items of that type => number
   // Example => Melitta => return 3
   // Example => Illy => return 1
 // LOGIC:
-// - Habe Variable, die die Anzahl Items speichert
-// - LOOPE durch Array
-// - Checke jedes item ob es das gesuchte Item ist
-// - Wenn ja: zähle eins hoch
-// - wenn nein: weitermachen mit der loop => gar nichts machen
-// - Ende der Function => return die Anzahl gefundener Items
+// - X Habe Variable, die die Anzahl Items speichert
+// - X LOOPE durch Array
+// - X Checke jedes item ob es das gesuchte Item ist
+// - X Wenn ja: zähle eins hoch
+// - X wenn nein: weitermachen mit der loop => gar nichts machen => kein Else
+// - X Ende der Function => return die Anzahl gefundener Items
+
+const getCoffeeCount = (arr, coffeeName) => {
+
+  let amount = 0
+
+  for(let i=0; i<arr.length; i++) {
+
+    if(arr[i] === coffeeName) {
+      amount += 1 // amount++ || amount = amount + 1
+    }
+  }
+
+  return amount
+}
+
+let result = getCoffeeCount(allCoffees, "Melitta") 
+console.log( result ) // expect: 3
+
+result = getCoffeeCount(allCoffees, "Milk") 
+console.log( result ) // expect: 0
+
+result = getCoffeeCount(allCoffees, "Tschibo") 
+console.log( result ) // expect: 1
