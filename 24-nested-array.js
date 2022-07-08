d316d42c18dcda2fcb1ac0fc83f2f246fd5778fa
losrobbos <losrobbos@web.de>
2022-07-08 11:44:57 +0200

    Nested looping sample

---
+++ 24-nested-array.js
@@ -0,0 +1,31 @@
+const coffees = [
+  { name: "Crema", flavors: ["Caramel", "Choco"] },
+  { name: "Espresso", flavors: [] },
+  { name: "Cappuccino", flavors: ["Choco", "Hazelnut"] },
+]
+const users = [
+  { name: "Eleni", hobbies: ["Coding", "Hulahoop"] },
+  { name: "Elvira", hobbies: ["Coding", "Gaming" ]}
+]
+
+// OUTER LOOP => loop through main items
+for(let user of users) {
+  console.log( user.name )
+  const hobbies = user.hobbies
+  // INNER LOOP => loop through details
+  for(let hobby of hobbies) {
+    console.log("-", hobby)
+  }
+}
+
+
+console.log()
+
+for(let i=0; i<coffees.length; i++) {
+  const coffeeAktuell = coffees[i] // save current item in variable
+  console.log(coffeeAktuell.name)
+
+  for(let j=0; j<coffeeAktuell.flavors.length; j++) {
+    console.log("-", coffeeAktuell.flavors[j])
+  }
+}
